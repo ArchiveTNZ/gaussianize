1e0ccaa4497422953ce85ddc78bb4fadaa14af1e
Abram Spamers <abram.spamers@team.telstra.com>
2019-07-19 10:52:48 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name='gaussianize',
-    version='0.1',
+    version='1.0-SNAPSHOT',
     url='https://github.com/Cognevo/gaussianize',
     license='MIT',
     author='Abram Spamers',
     author_email='abram.spamers@team.telstra.com',
     install_requires=[
-        'numpy', 'scipy', 'sklearn'
+        'numpy', 
+        'scipy', 
+        'sklearn'
     ],
     description='A fork of the versteeg gaussianize library'
 )
