117c92ae89da3227fefbed1445d704f88e4afaad
Abram Spamers <abram.spamers@team.telstra.com>
2019-07-19 12:21:02 +1200

    Add root to packages

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='gaussianize',
     version='1.0-SNAPSHOT',
+    packages=[''],
     url='https://github.com/Cognevo/gaussianize',
     license='MIT',
     author='Abram Spamers',
